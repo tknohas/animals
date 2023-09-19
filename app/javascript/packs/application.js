87aa3c307861fb2f9e2fda41c70476343c2db0f5
{tknohas} <{taka462411@gmail.com}>
2023-09-19 21:07:48 +0900

    swiper.jsの削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,13 +15,6 @@ ActiveStorage.start()
 import "bootstrap";
 import "../stylesheets/application.scss";
 
-require("@rails/ujs").start()
-//require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('./preview')
-require('./swiper')
-
 
 //= require underscore
 //= require gmaps/google
