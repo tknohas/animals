ab6c4ca644a5eccb20f0c21adbe2e0d3789dd3aa
{tknohas} <{taka462411@gmail.com}>
2023-09-29 14:43:03 +0900

    home_controllerにて新着投稿一覧の表示件数を9件に制限

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
     @tags = Tag.all
-    @animals = params[:name].present? ? Tag.find(params[:name]).animals : Animal.all
-    @animal = Animal.limit(10).order("id DESC ")
+    @animals = params[:name].present? ? Tag.find(params[:name]).animals : Animal.limit(9).order("id DESC")
   end
 end
