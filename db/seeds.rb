fab34ff080635ae3cb6c5405497798dd0efa3322
{tknohas} <{taka462411@gmail.com}>
2023-10-08 03:33:55 +0900

    heroku上でtag.nameが表示されない問題の解消

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-animal = Genre.create(name:  'みなさんのペットたち')
-
-dog, cat, small_animal = animal.children.create(
-  [
-    { name: 'ワンちゃん' },
-    { name: 'ネコちゃん' },
-    { name: '小動物・ほか' }
-  ]
-)
-
-['トイプードル', 'ミニチュアダックスフンド',  'ボーダーコリー', 'シェットランドシープドッグ',  'チワワ',  '柴犬',  'パグ',  'ミニチュアダックスフンド',  'ゴールデンレトリバー',  'その他'].each do |name|
-  dog.children.create(name: name)
-end
-
-['マンチカン', 'ロシアンブルー',  'アメリカンショートヘア', 'スコティッシュフォールド',  'ラグドール',  'サイベリアン',  'ベンガル',  'その他'].each do |name|
-  cat.children.create(name: name)
-end
-
-['うさぎ', 'ハムスター',  'フェレット', 'トリ',  'トカゲ',  'ヘビ', 'サル', '観賞魚', 'カメ'  'その他'].each do |name|
-  small_animal.children.create(name: name)
-end
-
-
 Tag
 Tag.create([
     { name: 'ワンちゃん' },
